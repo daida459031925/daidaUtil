07624894cddb0bad38d8178e97d0122c5820b5a7
戴达_上官爱 <daida459031925@163.com>
2020-01-16 15:11:59 +0800

    添加注释

--- src/main/java/com/daida/www/string/StringTool.java
+++ src/main/java/com/daida/www/string/StringTool.java
@@ -5,6 +5,7 @@ import java.util.function.Predicate;
 
 public class StringTool {
 
+    //字符串的长度是否为null或者""
     public static Predicate<String> StringLengthNull = (x) -> Optional.ofNullable(x).orElse("").trim().length()>0;
 
 }
